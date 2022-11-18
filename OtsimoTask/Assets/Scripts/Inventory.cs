9962f04e360b8ccef7f79d3909f385a8479d95ea
ceren98 <cereneycan98@gmail.com>
2022-11-18 19:21:24 +0300

    Slots by name

--- OtsimoTask/Assets/Scripts/Inventory.cs
+++ OtsimoTask/Assets/Scripts/Inventory.cs
@@ -7,8 +7,15 @@ using TMPro;
 
 public class Inventory : MonoBehaviour, IHasChanged
 {
+    [Header("Slots")]
     [SerializeField] Transform slots;
-    [SerializeField] Text inventoryText;
+    //[SerializeField] Text inventoryText;
+
+    [Header("Texts")]
+    [SerializeField] Text bottomText;
+    [SerializeField] Text upText;
+    [SerializeField] Text leftText;
+    [SerializeField] Text rightText;
     private void Start()
     {
         HasChanged();
@@ -16,19 +23,71 @@ public class Inventory : MonoBehaviour, IHasChanged
     #region IHasChanged Implementation
     public void HasChanged()
     {
-        System.Text.StringBuilder builder = new System.Text.StringBuilder();
-        builder.Append(" - ");
+      
+       // builder.Append("  ");
         foreach(Transform slotTransform in slots)
         {
+            System.Text.StringBuilder builder = new System.Text.StringBuilder();
             GameObject item = slotTransform.GetComponent<Slot>().item;
-            if (item)
+           
+            if (slotTransform.name == "SlotBottom")
             {
-                builder.Append(item.name);
-                builder.Append(" - ");
-            }
-        }
+                if (item)
+                {
+                    builder.Append(item.name);
+                    //builder.Append("  ");
+                   
+                    bottomText.text = builder.ToString();
+                }
+                else
+                {
+                  
+                    bottomText.text = "";
 
-        inventoryText.text = builder.ToString();
+                }
+            }
+            else if (slotTransform.name == "SlotUp")
+            {
+                if (item)
+                {
+                    builder.Append(item.name);
+                    upText.text = builder.ToString();
+                }
+                else
+                {
+                   
+                    upText.text = "";
+                }
+            }
+            else if(slotTransform.name == "SlotLeft")
+            {
+                if(item)
+                {
+                    builder.Append(item.name);
+                    leftText.text = builder.ToString();
+                }
+                else
+                {
+                    leftText.text = "";
+                }
+            }
+            else if(slotTransform.name == "SlotRight")
+            {
+                if (item)
+                {
+                    builder.Append(item.name);
+                    rightText.text = builder.ToString();
+                }
+                else
+                {
+                    rightText.text = "";
+                   // builder.Append("  ");
+                }
+            }
+
+        } 
+        
+        // inventoryText.text = builder.ToString();
     }
     #endregion
 }
